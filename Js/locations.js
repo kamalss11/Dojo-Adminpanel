78f01fb27d8732a2e55e61ff959a0d1b6c881bb2
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-28 21:59:27 +0530

    Add files via upload

--- Js/locations.js
+++ Js/locations.js
@@ -9,3 +9,9 @@ function en(){
     window.localStorage.setItem("TDocId",0)
     window.location.assign("https://adminpanel-dojo.netlify.app/entity")
 }
+
+var bars = document.querySelector(".bars")
+
+bars.addEventListener("click",function(){
+    bars.classList.toggle("active")
+})
