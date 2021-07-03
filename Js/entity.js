dd9b2e0e09bfef9af4d5507894955481dd465661
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-07-03 15:27:24 +0530

    Add files via upload

--- Js/entity.js
+++ Js/entity.js
@@ -220,6 +220,8 @@ function append(val){
         li.textContent = `${doc.name}`
         li.id = doc.id
         li.setAttribute("price",`${doc.price}`)
+        let price = document.createElement("span")
+        price.innerHTML = doc.Price
         let button = document.createElement("button")
         button.id = `${doc.id}`
         button.innerHTML = "Edit"
