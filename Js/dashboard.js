82e61a6bbae38b9dfd83fe525a110ccab9c5c6ac
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-07-06 22:00:55 +0530

    Add files via upload

--- Js/dashboard.js
+++ Js/dashboard.js
@@ -1,8 +1,14 @@
 var firestore = firebase.firestore()
 var studios = firestore.collection("Studios")
 var trainers = firestore.collection("Online")
+var docs = 0
+var dbRef = firebase.database().ref("Feedbacks");
 var tots = document.querySelector(".tns")
 var tott = document.querySelector(".tnt")
+var add = document.querySelector(".adbtn")
+var pop = document.querySelector(".pop")
+var cls = document.querySelector(".pop .close")
+var fd = document.querySelector(".fd")
 console.log(studios,trainers)
 
 window.onload = () =>{
@@ -23,4 +29,30 @@ window.onload = () =>{
         console.log("Total no. of trainers => " + count)
         tott.innerHTML = count
     })
+    
+    dbRef.get().then((snapshot) => {
+        console.log(snapshot.val())
+        if (snapshot.exists()) {
+            snapshot.forEach((doc)=>{
+                console.log(doc.val())
+                ++docs
+                let li = document.createElement("li")
+                li.innerHTML = `${docs}. ${doc.val().Feedback}`
+                fd.append(li)
+            })
+        } 
+        else {
+            alert("No data available");
+        }
+    }).catch((error) => {
+      console.error(error);
+    });
 }
+
+add.addEventListener("click",function(){
+    pop.classList.add("active")
+})
+
+cls.addEventListener("click",function(){
+    pop.classList.remove("active")
+})
