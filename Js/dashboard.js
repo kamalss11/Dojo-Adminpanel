6a67c04dc049f68220c6d4be4d204d63b52ffd37
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-09-26 08:19:09 +0530

    Add files via upload

--- Js/dashboard.js
+++ Js/dashboard.js
@@ -15,7 +15,7 @@ var fdli
 console.log(studios,trainers)
 var wel = document.querySelector(".wel")
 
-var bnr = firebase.database().ref("SlideBanner");
+var bnr = firebase.database().ref("SlideBanner").child("Banner1");
 
 window.onload = () =>{
     firebase.auth().onAuthStateChanged(function(user) {
