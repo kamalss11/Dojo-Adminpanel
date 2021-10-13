41e02df595fdbcc330ae3340b8488d58e66ee970
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-10-13 19:19:34 +0530

    Add files via upload

---
+++ Js/add_banner.js
@@ -0,0 +1,6 @@
+var firestore = firebase.firestore()
+
+var img = document.getElementById('img')
+var ph = document.getElementById('ph')
+var on = document.getElementById('on')
+var url = document.getElementById('url')
