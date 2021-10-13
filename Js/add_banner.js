b52c04e96386dd588c4f4c63df8c299a5d8c274a
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-10-13 19:20:37 +0530

    Add files via upload

--- Js/add_banner.js
+++ Js/add_banner.js
@@ -4,3 +4,27 @@ var img = document.getElementById('img')
 var ph = document.getElementById('ph')
 var on = document.getElementById('on')
 var url = document.getElementById('url')
+
+var bnr = firebase.database().ref("SliderBanner");
+
+bnr.get().then((snapshot) => {
+    console.log(snapshot.val())
+    if (snapshot.exists()) {
+        snapshot.forEach((doc)=>{
+            let div = document.createElement("div")
+            div.className = "swiper-slide"
+            let a = document.createElement("a")
+            a.href = `${doc.val().url}`
+            a.target = "_blank"
+            let img = document.createElement("img")
+            img.src = `${doc.val().image}`
+            a.append(img)
+            div.append(a)
+            console.log(doc.val().image)
+            banners.append(div)
+        })
+    }
+    else{
+        console.log("No data found")
+    }
+})
