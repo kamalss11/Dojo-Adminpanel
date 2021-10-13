525a9598125c2091e719c293d5daf0f7a8c33865
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-10-13 19:41:19 +0530

    Add files via upload

--- Js/add_banner.js
+++ Js/add_banner.js
@@ -4,27 +4,41 @@ var img = document.getElementById('img')
 var ph = document.getElementById('ph')
 var on = document.getElementById('on')
 var url = document.getElementById('url')
+var submit = document.getElementById('submit')
+var imgr = document.getElementById('imgr')
 
 var bnr = firebase.database().ref("SliderBanner");
-
+let no = 0
 bnr.get().then((snapshot) => {
     console.log(snapshot.val())
     if (snapshot.exists()) {
         snapshot.forEach((doc)=>{
-            let div = document.createElement("div")
-            div.className = "swiper-slide"
-            let a = document.createElement("a")
-            a.href = `${doc.val().url}`
-            a.target = "_blank"
-            let img = document.createElement("img")
-            img.src = `${doc.val().image}`
-            a.append(img)
-            div.append(a)
-            console.log(doc.val().image)
-            banners.append(div)
+            no = no + 1
         })
     }
     else{
-        console.log("No data found")
+        no = 0
     }
+})
+
+function im(){
+    ser.style.fontSize = "13px"
+    if(img.value == ""){
+        sbtn.classList.add("active")
+        img.style.borderColor = "red"
+        imgr.innerHTML = "This field is required"
+        imgr.style.display = "block"
+        imgr.style.color = "red"
+    }
+
+    else{
+        sbtn.classList.remove("active")
+        img.style.borderColor = "#80808059"
+        imgr.innerHTML = ""
+        imgr.style.display = "none"
+    }
+}
+
+sbtn.addEventListener('click',async function(){
+    await im()
 })
