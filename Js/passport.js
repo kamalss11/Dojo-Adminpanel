3c0824d84f237f81f63be67553e419a33f9bd32b
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-11-15 19:00:16 +0530

    Add files via upload

--- Js/passport.js
+++ Js/passport.js
@@ -1,3 +1,34 @@
 var firestore = firebase.firestore()
-var studios = firestore.collection("Activities")
-var studios_list = document.querySelector(".list-1")
+var activities = firestore.collection("Activities")
+var passportli = document.querySelector(".list-1")
+var wel = document.querySelector(".wel")
+
+window.onload = ()=>{
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (!user) {
+            location.assign("https://adminpanel-dojo.netlify.app/")
+        } 
+    
+        else{
+            wel.innerHTML += user.phoneNumber
+        }
+    })
+
+    activities.get().then((querySnapShot)=>{
+        querySnapShot.forEach((doc)=>{
+            console.log(doc.data(),doc.id)
+            let li = document.createElement("li")
+            li.textContent = `${doc.data().ActivityType}`
+            li.id = doc.id
+            let button = document.createElement("button")
+            button.id = `${doc.id}`
+            button.innerHTML = "Edit"
+            let span = document.createElement("span")
+            span.classList = "material-icons"
+            span.innerHTML = "edit"
+            button.append(span)
+            li.append(button)
+            activities.append(li)
+        })
+    })        
+}
